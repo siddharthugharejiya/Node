12bc0a93ee550cdf332f737d7eaf8f0578f143ba
siddharthugharejiya <sidhdharthugharejiya8182@gmail.com>
2024-09-26 11:56:02 +0530

    .

--- todo/app.js
+++ todo/app.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, 'views'));
 
 let todo = [];
 
-app.get('/', (req, res) => {
+
+app.get("/", (req, res) => {
     res.render("todo", { todo });
 });
+
 
 app.get("/add", (req, res) => {
     res.render("add");
@@ -16,29 +20,49 @@ app.get("/add", (req, res) => {
 
 app.post("/add", (req, res) => {
     const { task } = req.body;
-    todo.push({ id: todo.length + 1, task });
-    res.redirect('/');
+    todo.push({ id: todo.length + 1, task, completed: false });
+    res.redirect("/");
 });
 
-app.get("/edit/:id", (req, res) => {
+
+app.post("/complete/:id", (req, res) => {
     const id = parseInt(req.params.id);
-    const taskToEdit = todo.find(el => el.id === id);
-    res.render("edit", { taskToEdit });
+    const t = todo.find(el => el.id === id);
+    if (t) {
+        t.completed = !t.completed;
+    }
+    res.redirect("/");
 });
 
-app.post("/edit/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-    const updatedTask = req.body.task;
-    todo = todo.map(el => el.id === id ? { id: el.id, task: updatedTask } : el);
-    res.redirect('/');
-});
 
 app.post("/delete/:id", (req, res) => {
     const id = parseInt(req.params.id);
     todo = todo.filter(el => el.id !== id);
-    res.redirect('/');
+    res.redirect("/");
+});
+
+
+app.get("/edit/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const Edit = todo.find(el => el.id === id);
+    if (Edit) {
+        res.render("edit", { task: Edit });
+    } else {
+        res.redirect("/");
+    }
+});
+
+// Route to handle the form submission for editing a task
+app.post("/edit/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const { task } = req.body;
+    const t = todo.find(el => el.id === id);
+    if (t) {
+        t.task = task;
+    }
+    res.redirect("/");
 });
 
 app.listen(9595, () => {
-    console.log("server is running on port 9595");
+    console.log("Server is running on http://localhost:9595");
 });
