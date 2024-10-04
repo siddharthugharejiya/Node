8a6b0ec18b6a4f6ae310853065bcd52bbb3b2ef9
MadhavRaninga <madhavraninga3@gmail.com>
2024-10-04 17:34:14 +0530

    .

--- form/Model/userSchema.js
+++ form/Model/userSchema.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose")
-
 const Userschema = new mongoose.Schema({
     username:String,
     email:String,
     password:String
 })
 
-const Usemodel = mongoose.model("users",Userschema)
+const Usemodel = mongoose.model("products",Userschema)
 module.exports=Usemodel
--- form/app.js
+++ form/app.js
@@ -6,15 +6,40 @@ app.use(express.urlencoded({extended:true}))
 app.set("view engine","ejs")
 app.use(express.static("public"))
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render("form.ejs")
 })
 
-app.post("/",async(req,res)=>{
-      await Usemodel.create(req.body)
-})
-app.listen(9595,()=>{
-    connect()
-    console.log("server is runnig on 95959 port");
+app.get("/data", async (req, res) => {
+    const data = await Usemodel.find()
+    console.log(data);
     
+    res.render("data.ejs", { data })
+})
+
+app.post("/", async (req, res) => {
+    let data = await Usemodel.create(req.body)
+
+    res.redirect("/data")
+})
+
+app.get("/delete/:id", async (req, res) => {
+    const { id } = req.params
+    console.log(id);
+    await Usemodel.findByIdAndDelete(id)
+    console.log("delete successfully");
+    res.redirect("/data")
+})
+
+app.get("/edite/:id", async (req, res) => {
+    const { id } = req.params
+    let data = await Usemodel.findByIdAndUpdate(id);
+    res.render("edite.ejs", { data })
+    res.redirect("/data",{data})
+})
+
+
+app.listen(9595, () => {
+    connect()
+    console.log("server is running on 9595 port");
 })
