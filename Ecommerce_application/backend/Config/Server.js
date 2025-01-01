ffdd476daf14bd2c5ee30fc78589c2ff0d831006
siddharthugharejiya <sidhdharthugharejiya8182@gmail.com>
2025-01-01 12:15:37 +0530

    .

--- Ecommerce_application/backend/Config/Server.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-require('dotenv').config();
-const Connected = async() =>{
-       await mongoose.connect("mongodb+srv://multiera95:95@ecommerce.ofpkg.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=ecommerce")
-       console.log("Server is connected")
-}
-
-module.exports=Connected
--- Ecommerce_application/backend/Route/UserRoute.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const {Register, Login} = require('../Controller/UserController')
-const UserRote = express.Router()
-UserRote.post("/register",Register)
-UserRote.post("/login",Login)
-module.exports = UserRote
